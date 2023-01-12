a03cf6935f6a7d4bfcfc2bf03e3f07b233cb466e
dawkaka <yousiph77@gmail.com>
2023-01-12 01:02:20 +0000

    let's finish it tomorrow

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
-import Alert from './alert'
+import Image from "next/image"
+import Link from "next/link"
 import Footer from './footer'
 import Meta from './meta'
 
@@ -11,8 +12,22 @@ const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
+      <div className="min-h-screen max-w-[1400px] mx-[auto]">
+        <div>
+          <div>
+            <Image src={"/assets/npp-logo2.png"} width={70} height={70} alt="npp logo" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link href={"/"}>Home</Link>
+                <Link href={"/delegates"}>Delegates</Link>
+                <Link href={"/events"}>Events</Link>
+                <Link href={"/blog"}>Blog</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <main>{children}</main>
       </div>
       <Footer />
