33c251dcd51657a3092e7a5c742d521958648bfc
dawkaka <yousiph77@gmail.com>
2023-01-13 16:24:35 +0000

    hero changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,8 +4,8 @@ const Hero = () => {
     <header className="relative min-h-[calc(100vh-70px)] bg-[var(--p-color)]">
       <div className="absolute flex w-full h-full">
         <div className="w-[min(100%,1200px)] flex flex-col items-center mx-[auto]">
-          <h1 className="my-[auto] px-5 sm:px-0 leading-normal text-3xl xs:text-4xl sm:text-6xl md:7xl lg:text-8xl text-white font-bold text-center">
-            NPP, <span className="text-[red]">SALAGA SOUTH </span> CONSTITUENCY
+          <h1 className="relative my-[auto] px-5 sm:px-3 leading-normal text-3xl xs:text-4xl sm:text-6xl md:text-7xl text-white font-bold text-center">
+            <span className="slide-l">NPP,</span> <span className="text-[red] drop">SALAGA SOUTH </span> <span className="slide">CONSTITUENCY.</span>
           </h1>
         </div>
       </div>
